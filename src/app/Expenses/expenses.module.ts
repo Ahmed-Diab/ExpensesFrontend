497d61275506d785c1992fcb0729307047cd0e76
ahmed-diab <ahmedeiddiab@gmail.com>
2022-04-07 15:30:03 +0200

    add readme file

--- src/app/Expenses/expenses.module.ts
+++ src/app/Expenses/expenses.module.ts
@@ -3,21 +3,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ExpensesPage } from './expenses.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { ExpensesPageRoutingModule } from './expenses-routing.module';
-import { ExpenseFormPage } from '../expense-form/expense-form.page';
 import { ExpenseFormPageModule } from '../expense-form/expense-form.module';
  @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    ExpensesPageRoutingModule,
+     ExpensesPageRoutingModule,
     ExpenseFormPageModule
   ],
   declarations: [ExpensesPage],
-  schemas:[],
-  providers:[DatePipe]
+  schemas: [],
+  providers: [DatePipe]
 })
-export class ExpensesPageModule {}
+export class ExpensesPageModule { }
